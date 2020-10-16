2e7e1272307dc552c76f547f7c592f5e505b4f0c
Nebrass Lamouchi <lnibrass@gmail.com>
2020-10-17 00:16:22 +0200

    Add Tests for quarkushop-product microservice

--- quarkushop-product/src/test/java/com/targa/labs/quarkushop/product/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.targa.labs.quarkushop.product;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
